c358c4e941145193877b3801f04f6644e7464c5b
JennyCGT <jenniferguayta@gmail.com>
2023-11-18 01:26:52 -0500

    Add timeout

---
+++ app/src/main/java/com/jennycgt/androidVAD/StatusChangeDetector.kt
@@ -0,0 +1,46 @@
+package com.jrtec.grabadora
+
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+
+class StatusChangeDetector(private val onChangeTimeout: Long = 10000,
+                           private val callback: () -> Unit) {
+    private val handler = Handler(Looper.getMainLooper())
+    private var lastValue: String? = null
+
+
+    private val runnable = Runnable {
+        // Check if the variable hasn't changed its value for the specified timeout
+        if (lastValue == "noise") {
+            // Execute your function here
+            executeFunction()
+        }
+    }
+
+    fun startMonitoring() {
+        Log.i("STATUS","************************* START MONITORING ****************************************")
+        handler.postDelayed(runnable, onChangeTimeout)
+    }
+
+    fun stopMonitoring() {
+        handler.removeCallbacks(runnable)
+        lastValue = null
+    }
+
+    fun updateVariable(value: String) {
+        if (lastValue != value) {
+            lastValue = value
+//            Log.i("STATUS", "New Value $value")
+            handler.removeCallbacks(runnable)
+            handler.postDelayed(runnable, onChangeTimeout)
+        }
+    }
+
+    private fun executeFunction() {
+        // Implement your desired functionality here
+        // This function will be called if the variable hasn't changed its value in the specified timeout
+        Log.i("STATUS","************************ STOP MONITORING *****************************************")
+        callback()
+    }
+}
